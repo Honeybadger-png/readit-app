77adef90c58e91799087ebb9b235aea38eb7b115
Honeybadger-png <9emertgul@gmail.com>
2026-03-24 11:40:48 +0300

    basic setup

---
+++ readit-api/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express()
+const port = process.env.PORT || 2000;
+
+app.get('/',(req,res)=> {
+    res.send('Hello World')
+})
+
+app.listen(port,()=>{
+    console.log(`App listening on port ${port}`);
+})
